cd4ab6e9c8584213afd2e372c660f426f76c3a11
Israel Manzo <israelmanzo814@gmail.com>
2019-07-31 21:10:18 -0400

    Updated persistant services class with context

--- TipCalc/TipCalc/Model/PersistanceServices.swift
+++ TipCalc/TipCalc/Model/PersistanceServices.swift
@@ -11,10 +11,32 @@ import CoreData
 
 class PersistanceServices {
     
+    static var context: NSManagedObjectContext {
+        return persitantContrainer.viewContext
+    }
+    
     static var persitantContrainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "TipCalc")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unceratain Error \(error)")
+            }
+            print(storeDescription)
+        })
         return container
     }()
+    
+    static func saveContext() {
+        let context = persitantContrainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                print("saved")
+            } catch let error {
+                fatalError("Error loading a container \(error)")
+            }
+        }
+    }
 }
 
 //struct Bill {
