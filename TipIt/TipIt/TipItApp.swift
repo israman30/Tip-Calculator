01b8923fe1948695cb6783b5abc00c39014068db
israman <israelmanzo814@gmail.com>
2022-04-17 21:07:06 -0400

    added new SwiftUI project for tip calculator

---
+++ TipIt/TipIt/TipItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipItApp.swift
+//  TipIt
+//
+//  Created by Israel Manzo on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct TipItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
