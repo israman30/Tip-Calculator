3ff7b9bfd66f6d89167094c1a4735b028f7116cd
israman <israelmanzo814@gmail.com>
2022-04-17 21:29:08 -0400

    updated header texfield input + values output

--- TipIt/TipIt/ContentView.swift
+++ TipIt/TipIt/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var input = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Enter value..", text: $input)
+                    Image(systemName: "dollarsign.circle.fill")
+                        .foregroundColor(.secondary)
+                }
+                .multilineTextAlignment(.trailing)
+                .font(.title)
+                
+                VStack {}.frame(width: UIScreen.main.bounds.size.width - 100, height: 5).background(Color.secondary)
+                
+                VStack {
+                    VStack(alignment: .trailing) {
+                        HStack {
+                            Spacer()
+                            Text("Tip")
+                                .foregroundColor(.secondary)
+                        }
+                        HStack {
+                            Spacer()
+                            Text("$0.0")
+                                .font(.system(size: 45, weight: .bold, design: .rounded))
+                        }
+                    }
+                    VStack(alignment: .trailing) {
+                        HStack {
+                            Spacer()
+                            Text("Total")
+                                .foregroundColor(.secondary)
+                        }
+                        HStack {
+                            Spacer()
+                            Text("$0.0")
+                                .font(.system(size: 45, weight: .bold, design: .rounded))
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .navigationTitle("Tip It")
+        }
     }
 }
 
