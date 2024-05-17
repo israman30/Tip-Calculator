219ff7326ab82c1d837639d8a6eababcacb12649
Israel Manzo <israelmanzo814@gmail.com>
2024-05-16 21:21:09 -0400

    added test for protocol calculations

--- TipCalc/TipCalcTests/TipCalcTests.swift
+++ TipCalc/TipCalcTests/TipCalcTests.swift
@@ -9,14 +9,54 @@
 import XCTest
 @testable import TipCalc
 
+// Mock ViewModel implementing ViewModelBillCalculationsProtocol
+class MockViewModel: ViewModelBillCalculationsProtocol {
+    var calculateTipCalled = false
+    var resetCalled = false
+    var splitBillCalled = false
+    
+    func calculateTip(with valueInput: UITextField, segment: UISegmentedControl, tipValue: UILabel, totalValue: UILabel) {
+        calculateTipCalled = true
+    }
+    
+    func reset(valueInput: UITextField, tipValue: UILabel, totalValue: UILabel, totalByPerson: UILabel, peopleQuantity: UILabel) {
+        resetCalled = true
+    }
+    
+    func splitBiil(people: UILabel, bill: Double, totalByPerson: UILabel) {
+        splitBillCalled = true
+    }
+}
+
 final class TipCalcTests: XCTestCase {
+    
+    var caclculations: CalculationsViewModel!
+    var mockViewModel: MockViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        caclculations = CalculationsViewModel()
+        mockViewModel = MockViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        caclculations = nil
+        mockViewModel = nil
+    }
+    
+    func test_MainBill_Initialization() {
+        XCTAssertEqual(caclculations.mainBill, 0.0, "Initial value of mainBill should be 0.0")
+    }
+    
+    func testViewModelBillCalculations() {
+        // When
+        mockViewModel.calculateTip(with: UITextField(), segment: UISegmentedControl(), tipValue: UILabel(), totalValue: UILabel())
+        mockViewModel.reset(valueInput: UITextField(), tipValue: UILabel(), totalValue: UILabel(), totalByPerson: UILabel(), peopleQuantity: UILabel())
+        mockViewModel.splitBiil(people: UILabel(), bill: 100.0, totalByPerson: UILabel())
+        
+        // Then
+        XCTAssertTrue(mockViewModel.calculateTipCalled, "calculateTip method should be called")
+        XCTAssertTrue(mockViewModel.resetCalled, "reset method should be called")
+        XCTAssertTrue(mockViewModel.splitBillCalled, "splitBill method should be called")
     }
 
     func testExample() throws {
