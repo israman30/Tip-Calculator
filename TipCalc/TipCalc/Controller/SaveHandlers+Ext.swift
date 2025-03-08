d436f4187353459c15dab6fe62c5f4cdf6315327
Israel Manzo <israelmanzo814@gmail.com>
2025-03-07 22:51:49 -0500

    calling function for accessibility announce

--- TipCalc/TipCalc/Controller/SaveHandlers+Ext.swift
+++ TipCalc/TipCalc/Controller/SaveHandlers+Ext.swift
@@ -22,6 +22,7 @@ extension MainController {
         handleResetFields()
         tableView.reloadData()
         if saveViewModel?.isTotastVisible == true {
+            displayAccessibilityToastMessage()
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseOut, animations: {
                     self.toastMessage.view.alpha = 1
