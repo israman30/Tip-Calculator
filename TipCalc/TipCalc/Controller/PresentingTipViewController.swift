f09c0196a47f0dcfa564dcf688d49459d7653567
Israel Manzo <israelmanzo814@gmail.com>
2026-02-28 16:09:07 -0500

    update presenting vc ui

--- TipCalc/TipCalc/Controller/PresentingTipViewController.swift
+++ TipCalc/TipCalc/Controller/PresentingTipViewController.swift
@@ -9,33 +9,54 @@ import UIKit
 import SwiftUI
 
 class PresentingTipViewController: UIViewController, TableViewProtocol, SetUIProtocol, SaveViewModelProtocol {
-    
+
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = NSLocalizedString("Saved Bills", comment: "Saved bills title")
-        label.font = .preferredFont(forTextStyle: .title2)
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
         label.textColor = .label
         return label
     }()
-    
-    let topView: UIView = {
+
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = LocalizedString.swipeToDeleteHint
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.textColor = .tertiaryLabel
+        return label
+    }()
+
+    private let topView: UIView = {
         let view = UIView()
         view.backgroundColor = .secondarySystemBackground
         return view
     }()
-    
+
+    private let topSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .separator.withAlphaComponent(0.5)
+        return view
+    }()
+
     var tableView: UITableView = {
         let tv = UITableView()
         tv.rowHeight = UITableView.automaticDimension
+        tv.estimatedRowHeight = 120
         tv.showsVerticalScrollIndicator = false
         tv.separatorColor = .clear
         tv.allowsSelection = false
+        tv.backgroundColor = .systemGroupedBackground
+        tv.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 24, right: 0)
         return tv
     }()
-    
-    let dismissButton: UIButton = {
+
+    private let dismissButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(systemName: Constant.xmark_circle), for: .normal)
+        let config = UIImage.SymbolConfiguration(pointSize: 22, weight: .medium)
+        button.setImage(UIImage(systemName: Constant.xmark_circle, withConfiguration: config), for: .normal)
+        button.tintColor = .secondaryLabel
+        button.accessibilityLabel = NSLocalizedString("Close", comment: "Close button")
+        button.accessibilityHint = NSLocalizedString("Dismiss saved bills list", comment: "Dismiss hint")
         return button
     }()
     
@@ -44,11 +65,11 @@ class PresentingTipViewController: UIViewController, TableViewProtocol, SetUIPro
     override func viewDidLoad() {
         super.viewDidLoad()
         saveViewModel = SaveViewModel()
-        tableView.reloadData()
         setUI()
         dismissButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
         tableViewHandlers()
         saveViewModel?.fetchItems()
+        tableView.reloadData()
     }
     
     deinit {
@@ -56,45 +77,60 @@ class PresentingTipViewController: UIViewController, TableViewProtocol, SetUIPro
     }
     
     func setUI() {
-        view.backgroundColor = .systemBackground
-        
+        view.backgroundColor = .systemGroupedBackground
+
         view.addSubViews(topView, tableView)
-        
+
         topView.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         topView.anchor(
-            top: view.topAnchor,
+            top: view.safeAreaLayoutGuide.topAnchor,
             left: view.leftAnchor,
             bottom: nil,
             right: view.rightAnchor,
-            size: .init(width: 0, height: 60)
+            size: .init(width: 0, height: 76)
         )
         tableView.anchor(
             top: topView.bottomAnchor,
             left: view.leftAnchor,
             bottom: view.bottomAnchor,
             right: view.rightAnchor,
-            padding: .init(top: 8, left: 16, bottom: 0, right: 16)
+            padding: .init(top: 0, left: 16, bottom: 0, right: 16)
         )
-        
-        topView.addSubViews(titleLabel, dismissButton)
-        
+
+        topView.addSubViews(titleLabel, subtitleLabel, dismissButton, topSeparatorView)
+
         titleLabel.anchor(
             top: topView.topAnchor,
             left: topView.leftAnchor,
-            bottom: topView.bottomAnchor,
+            bottom: nil,
             right: nil,
-            padding: .init(top: 12, left: 20, bottom: 12, right: 0)
+            padding: .init(top: 16, left: 20, bottom: 0, right: 0)
         )
-        
+        subtitleLabel.anchor(
+            top: titleLabel.bottomAnchor,
+            left: topView.leftAnchor,
+            bottom: nil,
+            right: nil,
+            padding: .init(top: 4, left: 20, bottom: 0, right: 0)
+        )
         dismissButton.anchor(
-            top: topView.topAnchor,
+            top: nil,
             left: nil,
+            bottom: nil,
+            right: topView.rightAnchor,
+            padding: .init(top: 0, left: 0, bottom: 0, right: 16),
+            size: .init(width: 44, height: 44)
+        )
+        dismissButton.centerYAnchor.constraint(equalTo: topView.centerYAnchor).isActive = true
+        topSeparatorView.anchor(
+            top: nil,
+            left: topView.leftAnchor,
             bottom: topView.bottomAnchor,
             right: topView.rightAnchor,
-            padding: .init(top: 8, left: 0, bottom: 8, right: 16),
-            size: .init(width: 44, height: 44)
+            padding: .zero,
+            size: .init(width: 0, height: 1.0 / UIScreen.main.scale)
         )
     }
     
