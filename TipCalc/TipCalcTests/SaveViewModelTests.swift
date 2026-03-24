79fc129aa5778192afd1b4cf80699922f8853cde
Israel Manzo <israelmanzo814@gmail.com>
2026-03-23 23:23:51 -0400

    update save view model test

--- TipCalc/TipCalcTests/SaveViewModelTests.swift
+++ TipCalc/TipCalcTests/SaveViewModelTests.swift
@@ -22,8 +22,8 @@ class MockViewController: UIViewController {
 }
 
 final class SaveViewModelTests: XCTestCase {
-    
-    var savingViewModel: SaveViewModel!
+
+    var sut: SaveViewModel!
     var valueInput: UITextField!
     var tipValue: UILabel!
     var totalValue: UILabel!
@@ -32,7 +32,7 @@ final class SaveViewModelTests: XCTestCase {
     var mockViewController: MockViewController!
 
     override func setUpWithError() throws {
-        savingViewModel = SaveViewModel()
+        sut = SaveViewModel()
         valueInput = UITextField()
         tipValue = UILabel()
         totalValue = UILabel()
@@ -42,7 +42,7 @@ final class SaveViewModelTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        savingViewModel = nil
+        sut = nil
         valueInput = nil
         tipValue = nil
         totalValue = nil
@@ -50,72 +50,272 @@ final class SaveViewModelTests: XCTestCase {
         splitPeopleQuantity = nil
         mockViewController = nil
     }
-    
-    func test_BillsArray_InitialState() {
-        // Then
-        XCTAssertTrue(savingViewModel.bills.isEmpty, "The bills array should initially be empty")
+
+    private func configureValidSaveInputs() {
+        valueInput.text = "50.00"
+        tipValue.text = "$5.00"
+        totalValue.text = "$55.00"
+        splitTotal.text = "$27.50"
+        splitPeopleQuantity.text = "2x"
     }
-    
+
+    // MARK: - Initial State
+
+    func test_BillsArray_InitialState() {
+        XCTAssertTrue(sut.bills.isEmpty, "The bills array should initially be empty")
+    }
+
+    func test_IsTotastVisible_InitialState() {
+        XCTAssertFalse(sut.isTotastVisible, "Toast should not be visible initially")
+    }
+
+    func test_SortedBills_WhenEmpty_ReturnsEmptyArray() {
+        XCTAssertTrue(sut.sortedBills.isEmpty)
+    }
+
+    // MARK: - Bills Array Operations
+
     func test_BillsArray_Add_Bill() {
-        // Given
         let bill = Bill(context: PersistanceServices.context)
         bill.input = String(100)
-        
-        // When
-        savingViewModel.bills.append(bill) // Simulate adding a bill to the array
-        
-        // Then
-        XCTAssertEqual(savingViewModel.bills.count, 1, "The bills array should contain one bill")
-        XCTAssertEqual(savingViewModel.bills.first?.input, String(100), "The bill in the array should have the correct amount")
+
+        sut.bills.append(bill)
+
+        XCTAssertEqual(sut.bills.count, 1)
+        XCTAssertEqual(sut.bills.first?.input, String(100))
     }
-    
+
     func test_BillsArray_Clear() {
-        // Given
         let bill1 = Bill(context: PersistanceServices.context)
         bill1.input = String(100)
         let bill2 = Bill(context: PersistanceServices.context)
         bill2.input = String(200)
-        
-        savingViewModel.bills.append(bill1)
-        savingViewModel.bills.append(bill2)
-        
-        // When
-        savingViewModel.bills.removeAll() // Simulate clearing the array
-        
-        // Then
-        XCTAssertTrue(savingViewModel.bills.isEmpty, "The bills array should be empty after clearing")
+        sut.bills.append(bill1)
+        sut.bills.append(bill2)
+
+        sut.bills.removeAll()
+
+        XCTAssertTrue(sut.bills.isEmpty)
     }
-    
+
+    // MARK: - Save - Empty / Invalid Input
+
     func test_Save_With_Empty_Input_ShowsAlert() {
-        // Given
         valueInput.text = ""
         tipValue.text = "$5.00"
         totalValue.text = "$55.00"
         splitTotal.text = "$27.50"
         splitPeopleQuantity.text = "2x"
-        
-        // When
-        savingViewModel.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
-        
-        // Then
-        XCTAssertNotNil(mockViewController.presentedAlert, "An alert should be presented when the input is empty")
-        XCTAssertEqual(mockViewController.presentedAlert?.title, "😵", "The alert title should be correct")
-        XCTAssertEqual(mockViewController.presentedAlert?.message, LocalizedString.no_value_to_be_saved, "The alert message should be correct")
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertNotNil(mockViewController.presentedAlert)
+        XCTAssertEqual(mockViewController.presentedAlert?.title, "😵")
+        XCTAssertEqual(mockViewController.presentedAlert?.message, LocalizedString.no_value_to_be_saved)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_Save_With_NilInput_ReturnsEarly_NoAlert() {
+        valueInput.text = nil
+        tipValue.text = "$5.00"
+        totalValue.text = "$55.00"
+        splitTotal.text = "$27.50"
+        splitPeopleQuantity.text = "2x"
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertNotNil(mockViewController.presentedAlert)
+        XCTAssertTrue(sut.bills.isEmpty)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_Save_With_NilTip_ReturnsEarly_NoAlert() {
+        valueInput.text = "50"
+        tipValue.text = nil
+        totalValue.text = "$55"
+        splitTotal.text = "$27.50"
+        splitPeopleQuantity.text = "2x"
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertNil(mockViewController.presentedAlert)
+        XCTAssertTrue(sut.bills.isEmpty)
     }
 
+    func test_Save_With_NilTotal_ReturnsEarly_NoAlert() {
+        valueInput.text = "50"
+        tipValue.text = "$5"
+        totalValue.text = nil
+        splitTotal.text = "$27.50"
+        splitPeopleQuantity.text = "2x"
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertNil(mockViewController.presentedAlert)
+        XCTAssertTrue(sut.bills.isEmpty)
+    }
+
+    func test_Save_With_NilSplitTotal_ReturnsEarly_NoAlert() {
+        configureValidSaveInputs()
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: nil, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertNil(mockViewController.presentedAlert)
+        XCTAssertTrue(sut.bills.isEmpty)
+    }
+
+    func test_Save_With_NilSplitPeopleQuantity_ReturnsEarly_NoAlert() {
+        configureValidSaveInputs()
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: nil, category: nil)
+
+        XCTAssertNil(mockViewController.presentedAlert)
+        XCTAssertTrue(sut.bills.isEmpty)
+    }
+
+    func test_Save_With_EmptySplitTotalText_ReturnsEarly_NoAlert() {
+        valueInput.text = "50"
+        tipValue.text = "$5"
+        totalValue.text = "$55"
+        splitTotal.text = nil
+        splitPeopleQuantity.text = "2x"
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertNil(mockViewController.presentedAlert)
+        XCTAssertTrue(sut.bills.isEmpty)
+    }
+
+    // MARK: - Save - Valid Input
+
+    func test_Save_With_ValidInput_SavesBill_AndSetsToastVisible() {
+        configureValidSaveInputs()
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertEqual(sut.bills.count, 1)
+        XCTAssertTrue(sut.isTotastVisible)
+        XCTAssertNil(mockViewController.presentedAlert)
+    }
+
+    func test_Save_With_ValidInput_ResignsFirstResponder() {
+        configureValidSaveInputs()
+        let window = UIWindow()
+        window.addSubview(valueInput)
+        valueInput.becomeFirstResponder()
+        XCTAssertTrue(valueInput.isFirstResponder)
+
+        sut.save(mockViewController, valueInput: valueInput, tipValue: tipValue, totalValue: totalValue, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertFalse(valueInput.isFirstResponder)
+    }
+
+    // MARK: - savingInLocalstorage
+
+    func test_SavingInLocalstorage_CreatesBillWithCorrectFormat() {
+        let input = "100.00"
+        let tip = "$18.00"
+        let total = "$118.00"
+        let splitTotal = "$59.00"
+        let splitPeopleQuantity = "2x"
+
+        sut.savingInLocalstorage(with: input, tip: tip, total: total, splitTotal: splitTotal, splitPeopleQuantity: splitPeopleQuantity, category: nil)
+
+        XCTAssertEqual(sut.bills.count, 1)
+        let bill = sut.bills.first!
+        XCTAssertEqual(bill.input, "$\(input) \(LocalizedString.initial_bill)")
+        XCTAssertEqual(bill.tip, "\(tip) tip")
+        XCTAssertEqual(bill.total, "\(total) total")
+        XCTAssertEqual(bill.splitTotal, splitTotal)
+        XCTAssertEqual(bill.splitPeopleQuantity, splitPeopleQuantity)
+        XCTAssertNotNil(bill.date)
+    }
+
+    func test_SavingInLocalstorage_WithNilCategory_UsesDefaultRestaurant() {
+        sut.savingInLocalstorage(with: "50", tip: "$5", total: "$55", splitTotal: nil, splitPeopleQuantity: nil, category: nil)
+
+        XCTAssertEqual(sut.bills.count, 1)
+        XCTAssertEqual(sut.bills.first?.category, BillCategory.restaurant.rawValue)
+    }
+
+    func test_SavingInLocalstorage_WithCustomCategory_UsesProvidedCategory() {
+        let category = BillCategory.bar.rawValue
+        sut.savingInLocalstorage(with: "50", tip: "$5", total: "$55", splitTotal: nil, splitPeopleQuantity: nil, category: category)
+
+        XCTAssertEqual(sut.bills.count, 1)
+        XCTAssertEqual(sut.bills.first?.category, category)
+    }
+
+    func test_SavingInLocalstorage_AppendsToBillsArray() {
+        sut.savingInLocalstorage(with: "50", tip: "$5", total: "$55", splitTotal: nil, splitPeopleQuantity: nil, category: nil)
+        sut.savingInLocalstorage(with: "100", tip: "$20", total: "$120", splitTotal: nil, splitPeopleQuantity: nil, category: nil)
+
+        XCTAssertEqual(sut.bills.count, 2)
+    }
+
+    // MARK: - sortedBills
+
+    func test_SortedBills_SortsByDate_Ascending() {
+        let bill1 = Bill(context: PersistanceServices.context)
+        bill1.date = "Mar 20, 2026"
+        bill1.input = "50"
+        let bill2 = Bill(context: PersistanceServices.context)
+        bill2.date = "Mar 23, 2026"
+        bill2.input = "100"
+        let bill3 = Bill(context: PersistanceServices.context)
+        bill3.date = "Mar 15, 2026"
+        bill3.input = "25"
+        sut.bills = [bill1, bill2, bill3]
+
+        let sorted = sut.sortedBills
+
+        XCTAssertEqual(sorted.map { $0.date ?? "" }, ["Mar 15, 2026", "Mar 20, 2026", "Mar 23, 2026"])
+    }
+
+    func test_SortedBills_HandlesNilDates() {
+        let billWithDate = Bill(context: PersistanceServices.context)
+        billWithDate.date = "Mar 23, 2026"
+        billWithDate.input = "50"
+        let billWithoutDate = Bill(context: PersistanceServices.context)
+        billWithoutDate.date = nil
+        billWithoutDate.input = "100"
+        sut.bills = [billWithDate, billWithoutDate]
+
+        let sorted = sut.sortedBills
+
+        XCTAssertEqual(sorted.count, 2)
+    }
+
+    // MARK: - displayToast
+
+    func test_DisplayToast_WhenToastVisible_SetsToFalse() {
+        sut.isTotastVisible = true
+
+        sut.displayToast(UIView())
+
+        XCTAssertFalse(sut.isTotastVisible)
+    }
+
+    func test_DisplayToast_WhenToastNotVisible_SetsToFalse() {
+        sut.isTotastVisible = false
+
+        sut.displayToast(UIView())
+
+        XCTAssertFalse(sut.isTotastVisible)
+    }
+
+    // MARK: - fetchItems
+
+    func test_FetchItems_DoesNotCrash() {
+        sut.fetchItems()
+        XCTAssertNotNil(sut.bills)
+    }
+
+    func test_FetchItems_UpdatesBillsArray() {
+        sut.savingInLocalstorage(with: "50", tip: "$5", total: "$55", splitTotal: nil, splitPeopleQuantity: nil, category: nil)
+        PersistanceServices.saveContext()
+
+        let freshViewModel = SaveViewModel()
+        freshViewModel.fetchItems()
+
+        XCTAssertGreaterThanOrEqual(freshViewModel.bills.count, 1)
+    }
 }
